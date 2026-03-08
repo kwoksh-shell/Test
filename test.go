1d3ffc104af5722b8de4517b3127a081b2165d1e
kwoksh-shell <1344920488@qq.com>
2026-03-08 15:50:00 +0800

    修改singleflight

--- test.go
+++ test.go
@@ -1,81 +1,114 @@
 package main
 
 import (
-	"container/list"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"strconv"
+	"strings"
+	"time"
 )
 
-type node struct {
-	key, value, freq int
+func Swap(nums []int, i, j int) {
+	tmp := nums[i]
+	nums[i] = nums[j]
+	nums[j] = tmp
 }
 
-type LFUCache struct {
-	capacity, size, minFreq int
-	keyMap                  map[int]*list.Element
-	freqMap                 map[int]*list.List
+func patition(nums []int, left, right int) int {
+	i, j := left, right
+	for i < j {
+		for i < j && nums[j] >= nums[left] {
+			j--
+		}
+		for i < j && nums[i] <= nums[left] {
+			i++
+		}
+		Swap(nums, i, j)
+	}
+	Swap(nums, left, i)
+	return i
 }
 
-func Constructor(capacity int) LFUCache {
-	return LFUCache{
-		capacity: capacity,
-		size:     0,
-		minFreq:  0,
-		keyMap:   map[int]*list.Element{},
-		freqMap:  map[int]*list.List{},
-	}
-}
-
-func (this *LFUCache) Get(key int) int {
-	elem, exist := this.keyMap[key]
-	if !exist {
-		return -1
-	}
-	n := elem.Value.(*node)
-	if this.freqMap[n.freq].Len() == 0 && this.minFreq == n.freq {
-		this.minFreq++
-	}
-	n.freq++
-	if this.freqMap[n.freq] == nil {
-		this.freqMap[n.freq] = list.New()
-	}
-	newElem := this.freqMap[n.freq].PushBack(n)
-	this.keyMap[key] = newElem
-	return n.value
-}
-
-func (this *LFUCache) Put(key, value int) {
-	if this.capacity <= 0 {
+func QuickSort(nums []int, left, right int) {
+	if left >= right {
 		return
 	}
-	if elem, exist := this.keyMap[key]; exist {
-		n := elem.Value.(*node)
-		this.freqMap[n.freq].Remove(elem)
-		if this.freqMap[n.freq].Len() == 0 && this.minFreq == n.freq {
-			this.minFreq++
-		}
-		n.freq++
-		if this.freqMap[n.freq] == nil {
-			this.freqMap[n.freq] = list.New()
-		}
-		newElem := this.freqMap[n.freq].PushBack(n)
-		this.keyMap[key] = newElem
-		return
+	pivotIndex := left + rand.Intn(right-left+1)
+	Swap(nums, pivotIndex, left)
+	pivot := patition(nums, left, right)
+	QuickSort(nums, left, pivot-1)
+	QuickSort(nums, pivot+1, right)
+}
+
+func mergeSort(nums []int) []int {
+	n := len(nums)
+	if n == 0 || n == 1 {
+		return nums
 	}
-	if this.size >= this.capacity {
-		victimLists := this.freqMap[this.minFreq]
-		victimElem := victimLists.Front()
-		victimNode := victimElem.Value.(*node)
-		delete(this.keyMap, victimNode.key)
-		victimLists.Remove(victimElem)
-		if victimLists.Len() == 0 {
-			delete(this.freqMap, this.minFreq)
+	mid := n / 2
+	left := mergeSort(nums[:mid])
+	right := mergeSort(nums[mid:])
+	return merge(left, right)
+}
+
+func merge(l1, l2 []int) []int {
+	res := []int{}
+	for len(l1) != 0 && len(l2) != 0 {
+		if l1[0] < l2[0] {
+			res = append(res, l1[0])
+			l1 = l1[1:]
+		} else {
+			res = append(res, l2[0])
+			l2 = l2[1:]
 		}
 	}
-	newNode := &node{key: key, value: value, freq: 1}
-	if this.freqMap[1] == nil {
-		this.freqMap[1] = list.New()
+	res = append(res, l1...)
+	res = append(res, l2...)
+	return res
+}
+
+func HeapSort(nums []int) []int {
+	heapSize := len(nums)
+	buildHeap(nums, heapSize)
+	for i := len(nums) - 1; i >= 0; i-- {
+		nums[i], nums[0] = nums[0], nums[i]
+		heapSize--
+		maxHeapfy(nums, 0, heapSize)
 	}
-	Elem := this.freqMap[1].PushBack(newNode)
-	this.keyMap[key] = Elem
-	this.minFreq = 1
-	this.size++
+	return nums
+}
+
+func buildHeap(nums []int, heapSize int) {
+	for i := heapSize/2 - 1; i >= 0; i-- {
+		maxHeapfy(nums, i, heapSize)
+	}
+}
+
+func maxHeapfy(nums []int, i, heapSize int) {
+	l, r, largest := i*2+1, i*2+2, i
+	if l < heapSize && nums[l] > nums[largest] {
+		largest = l
+	}
+	if r < heapSize && nums[r] > nums[largest] {
+		largest = r
+	}
+	if largest != i {
+		nums[i], nums[largest] = nums[largest], nums[i]
+		maxHeapfy(nums, largest, heapSize)
+	}
+}
+
+func main() {
+	nums := []int{}
+	rand.Seed(time.Now().UnixNano())
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	numLine := strings.Fields(scanner.Text())
+	for i := 0; i < len(numLine); i++ {
+		num, _ := strconv.Atoi(numLine[i])
+		nums = append(nums, num)
+	}
+	fmt.Println(HeapSort(nums))
 }
