2f7de8b88d98074895075d234544e7ff64f722ad
kwoksh-shell <1344920488@qq.com>
2026-03-14 13:44:14 +0800

    :链表版本

--- test.go
+++ test.go
@@ -2,193 +2,157 @@ package main
 
 import (
 	"bufio"
-	"container/list"
 	"fmt"
 	"os"
 	"strconv"
 	"strings"
 )
 
-type TreeNode struct {
-	Val         int
-	Left, Right *TreeNode
+type ListNode struct {
+	Val  int
+	Next *ListNode
 }
 
-func buildTree(node []string) *TreeNode {
-	if len(node) == 0 || node[0] == "null" {
+func buildList(nums []int) *ListNode {
+	dummy := &ListNode{}
+	cur := dummy
+	for _, num := range nums {
+		cur.Next = &ListNode{Val: num}
+		cur = cur.Next
+	}
+	return dummy.Next
+}
+
+func buildInterSectList(interSectval, skipA, skipB int, l1, l2 []int) (*ListNode, *ListNode) {
+	var commonHead *ListNode
+	if interSectval != 0 {
+		commonNum := l1[skipA:]
+		commonHead = buildList(commonNum)
+	}
+	headA := buildList(l1[:skipA])
+	if headA != nil && commonHead != nil {
+		curA := headA
+		for curA.Next != nil {
+			curA = curA.Next
+		}
+		curA.Next = commonHead
+	} else if commonHead != nil {
+		headA = commonHead
+	}
+	headB := buildList(l2[skipB:])
+	if headB != nil && commonHead != nil {
+		curB := headB
+		for curB.Next != nil {
+			curB = curB.Next
+		}
+		curB.Next = commonHead
+	} else if commonHead != nil {
+		headB = commonHead
+	}
+	return headA, headB
+}
+
+func Check(l1, l2 *ListNode) *ListNode {
+	p1, p2 := l1, l2
+	for p1 != p2 {
+		if p1 != nil {
+			p1 = p1.Next
+		} else {
+			p1 = l2
+		}
+		if p2 != nil {
+			p2 = p2.Next
+		} else {
+			p2 = l1
+		}
+	}
+	return p1
+}
+
+func PrintList(head *ListNode) {
+	outPut := []string{}
+	for head != nil {
+		outPut = append(outPut, strconv.Itoa(head.Val))
+		head = head.Next
+	}
+	fmt.Println(strings.Join(outPut, "->") + "->nil")
+}
+
+func buildCycleList(head *ListNode, pos int) *ListNode {
+	if head == nil || pos < 0 {
+		return head
+	}
+	lastNode := head
+	listLen := 1
+	for lastNode.Next != nil {
+		lastNode = lastNode.Next
+		listLen++
+	}
+	if pos >= listLen {
+		return head
+	}
+	cycleEntry := head
+	for i := 0; i < pos; i++ {
+		cycleEntry = cycleEntry.Next
+	}
+	lastNode.Next = cycleEntry
+	return head
+}
+
+func HasCycle(head *ListNode) *ListNode {
+	if head == nil {
 		return nil
 	}
-	val, _ := strconv.Atoi(node[0])
-	root := &TreeNode{Val: val}
-	queue := []*TreeNode{root}
-	index := 1
-	for index < len(node) && len(queue) > 0 {
-		n := queue[0]
-		queue = queue[1:]
-		if index < len(node) {
-			if node[index] != "null" {
-				left_Val, _ := strconv.Atoi(node[index])
-				n.Left = &TreeNode{Val: left_Val}
-				queue = append(queue, n.Left)
-			}
-			index++
-		}
-		if index < len(node) {
-			if node[index] != "null" {
-				right_Val, _ := strconv.Atoi(node[index])
-				n.Right = &TreeNode{Val: right_Val}
-				queue = append(queue, n.Right)
-			}
-			index++
+	slow, fast := head, head
+	hasCycle := false
+	for fast.Next != nil && fast.Next.Next != nil {
+		fast = fast.Next.Next
+		slow = slow.Next
+		if slow == fast {
+			hasCycle = true
+			break
 		}
 	}
-	return root
-}
-
-func findNode(root *TreeNode, val int) *TreeNode {
-	if root == nil {
+	if hasCycle == false {
 		return nil
 	}
-	queue := []*TreeNode{root}
-	for len(queue) > 0 {
-		size := len(queue)
-		for i := 0; i < size; i++ {
-			node := queue[0]
-			queue = queue[1:]
-			if node.Val == val {
-				return node
-			}
-			if node.Left != nil {
-				queue = append(queue, node.Left)
-			}
-			if node.Right != nil {
-				queue = append(queue, node.Right)
-			}
-		}
+	fast = head
+	for slow != fast {
+		slow = slow.Next
+		fast = fast.Next
 	}
-	return nil
-}
-
-func PrintTree(root *TreeNode) {
-	if root == nil {
-		fmt.Println("null")
-	}
-	queue := []*TreeNode{root}
-	ans := []int{}
-	for len(queue) > 0 {
-		size := len(queue)
-		for i := 0; i < size; i++ {
-			node := queue[0]
-			queue = queue[1:]
-			ans = append(ans, node.Val)
-			if node.Left != nil {
-				queue = append(queue, node.Left)
-			}
-			if node.Right != nil {
-				queue = append(queue, node.Right)
-			}
-		}
-	}
-	fmt.Println(ans)
-}
-
-func sortedArrayToBST(nums []string) *TreeNode {
-	n := len(nums)
-	if n == 0 {
-		return nil
-	}
-	mid := n / 2
-	val, _ := strconv.Atoi(nums[mid])
-	root := &TreeNode{Val: val}
-	root.Left = sortedArrayToBST(nums[:mid])
-	root.Right = sortedArrayToBST(nums[mid+1:])
-	return root
-}
-
-func inorderTraversal(root *TreeNode) []int {
-	if root == nil {
-		return nil
-	}
-	stack := list.New()
-	stack.PushBack(root)
-	ans := []int{}
-	for stack.Len() > 0 {
-		e := stack.Back()
-		stack.Remove(e)
-		if e.Value == nil {
-			e = stack.Back()
-			stack.Remove(e)
-			node := e.Value.(*TreeNode)
-			ans = append(ans, node.Val)
-			continue
-		}
-		node := e.Value.(*TreeNode)
-		if node.Right != nil {
-			stack.PushBack(node.Right)
-		}
-		stack.PushBack(node)
-		stack.PushBack(nil)
-		if node.Left != nil {
-			stack.PushBack(node.Left)
-		}
-	}
-	return ans
-}
-
-func pathSum(root *TreeNode, targetSum int) int {
-	if root == nil {
-		return 0
-	}
-	res := rootSum(root, targetSum)
-	res += pathSum(root.Left, targetSum)
-	res += pathSum(root.Right, targetSum)
-	return res
-}
-
-func rootSum(root *TreeNode, targetSum int) int {
-	if root == nil {
-		return 0
-	}
-	res := 0
-	val := root.Val
-	if val == targetSum {
-		res++
-	}
-	res += rootSum(root.Left, targetSum-val)
-	res += rootSum(root.Right, targetSum-val)
-	return res
-}
-
-func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	if root == nil || root == p || root == q {
-		return root
-	}
-	left := lowestCommonAncestor(root.Left, p, q)
-	right := lowestCommonAncestor(root.Right, p, q)
-	if left != nil && right != nil {
-		return root
-	} else if left == nil && right == nil {
-		return nil
-	} else if left == nil {
-		return right
-	} else {
-		return left
-	}
+	return slow
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Scan()
 	numLine := strings.Fields(scanner.Text())
-	nums := []string{}
+	nums := []int{}
 	for i := 0; i < len(numLine); i++ {
-		nums = append(nums, numLine[i])
+		num, _ := strconv.Atoi(numLine[i])
+		nums = append(nums, num)
 	}
-	root := buildTree(nums)
-	var p, q int
-	fmt.Scan(&p, &q)
-	Tree1 := findNode(root, p)
-	Tree2 := findNode(root, q)
-	result := lowestCommonAncestor(root, Tree1, Tree2)
-	fmt.Println(result.Val)
+	head := buildList(nums)
+	PrintList(head)
+	var pos int
+	fmt.Scan(&pos)
+	l1 := buildCycleList(head, pos)
+	fmt.Println(HasCycle(l1).Val)
+
+	/* 相交链表
+	scanner.Scan()
+	numLine2 := strings.Fields(scanner.Text())
+	nums2 := []int{}
+	for i := 0; i < len(numLine2); i++ {
+		num, _ := strconv.Atoi(numLine2[i])
+		nums2 = append(nums2, num)
+	}
+
+	var interSectval, skipA, skipB int
+	fmt.Scan(&interSectval, &skipA, &skipB)
+	l1, l2 := buildInterSectList(interSectval, skipA, skipB, nums, nums2)
+	PrintList(Check(l1, l2))
+	*/
+
 }
